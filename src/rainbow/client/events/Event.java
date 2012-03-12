a8b459dc648cf9c63d4a599cf54c8efd9456805e
Fred Shih <ffred@sfu.ca>
2012-03-12 01:41:13 -0700

    Adding events to client code. Wrote md5 task which unhashes md5 sums, tested with sample from SchedulerServer.

---
+++ src/rainbow/client/events/Event.java
@@ -0,0 +1,11 @@
+package rainbow.client.events;
+
+import rainbowpc.Message;
+
+public abstract class Event {
+	public void run(Message msg) {
+		action(msg);
+	}
+
+	public abstract void action(Message msg);
+}
--- src/rainbow/client/worker/Worker.java
+++ src/rainbow/client/worker/Worker.java
@@ -3,11 +3,67 @@ package rainbow.client.worker;
 import rainbowpc.node.NodeProtocol;
 import rainbowpc.Message;
 import rainbowpc.RainbowException;
+import rainbow.client.events.Event;
+import java.util.TreeMap;
+import java.io.IOException;
 
 public class Worker extends Thread {
-	public Worker() {
+	public static final String TEST_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	
+	public TreeMap<String, Event> eventMapping;
+	private int id;
+	private NodeProtocol agent;
+
+	public Worker(int id, NodeProtocol agent) {
+		this.id = id;
+		this.agent = agent;
+		log("Worker spawned!");
 	}
 
 	public void run() {
+		log("Entering main execution loop...");
+		while (true) {
+			try {
+				Message message = agent.getMessage();
+				Event event = eventMapping.get(message.getMethod());
+				if (event != null) {
+					event.run(message);
+				} else {
+					warn(message.getMethod() + " event not recognized");
+				}
+				
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+	}
+
+	public void sendMessage(Message msg) {
+		try {
+			agent.sendMessage(msg);
+		} catch (IOException e) {
+			agent.shutdown();
+			warn("Message sending failed, aborting!");
+		}
+	}
+
+	public void setEventMapping(TreeMap<String, Event> eventMapping) {
+		this.eventMapping = eventMapping;
+	}
+
+	public void log(String msg) {
+		System.out.println("[====== Worker{" + 
+							id +
+							"} ======]: " +
+							msg
+		);
+	}
+
+	public void warn(String msg) {
+		System.out.println("[****** Worker{" + 
+							id +
+							"} ******]: " +
+							msg
+		);
 	}
 }
