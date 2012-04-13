398c80717a3f92ae05198cfefa46fb10d7686728
Aziz Zitouni <aziz0124@gmail.com>
2012-04-12 21:29:06 -0700

    Modifications to reduction function: 	- Adding begin string, and end string as parameters to the function Modifications to rainbow table: 	- Adding blocks support to generate rainbow tables for a specific block number

--- src/RainbowTable.java
+++ src/RainbowTable.java
@@ -8,228 +8,264 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
+import rainbow.scheduler.partition.AlphabetGenerator;
+import rainbow.scheduler.partition.PlaintextSpace;
+
 
 public class RainbowTable {
+
+	// The rainbow table is stored in a TreeMap
+	private TreeMap<String, String> microTable;
+	private int chainLength;
+	private int chainCount;
+	private String startingPlaintext;
+	private String endingPlaintext;
+	// For generating random integers
+	private Random randomGenerator;
 	
-		// The rainbow table is stored in a TreeMap
-		private TreeMap<String, String> microTable;
-		private int chainLength;
-		private int chainCount;
-		private String startingPlaintext;
-		// For generating random integers
-		private Random randomGenerator;
-		
-		private static final int MINLENGTH_PLAINTEXT = 3;
-		private static final int MAXLENGTH_PLAINTEXT = 10;
-		// Using only lowercase letters for testing
-		private static final int ALPHABETSIZE = 26;
-		
-		public RainbowTable(int chainLength, int chainCount, String startingPlaintext)
-		{
-			this.chainCount = chainCount;
-			this.chainLength = chainLength;
-			this.startingPlaintext = startingPlaintext;
-			
-			microTable = new TreeMap<String,String>();
-			//visitedPlaintexts = new HashMap<String,Integer>();
-			randomGenerator = new Random();
-		}
-		
-		public void generateMicroTable() throws NoSuchAlgorithmException
-		{
-			byte msgDigest[] = null;
-	        	MessageDigest algorithm = MessageDigest.getInstance("MD5");
-	        	for (int i = 0; i < chainCount; i++) {
-				// Store startingPlaintext in another variable to avoid getting modified
-				String value = startingPlaintext;
-				for (int j = 0; j < chainLength; j++) {
-					algorithm.reset();
-			    	algorithm.update(value.getBytes());
-			    	msgDigest= algorithm.digest();
-			    	value = ReductionFunction.reductionFunction(msgDigest);
-	            	}
-	        	// Add first plaintext of chain along with the last hash
-	        	microTable.put(startingPlaintext, getHex(msgDigest));
-	        	startingPlaintext = randomPlaintext();
-	        	// Keep generating random plaintexts until it finds one that haven't been used
-	        	while (microTable.containsKey(startingPlaintext))
-	        	{
-	            		startingPlaintext = randomPlaintext();
-	        	}
-	        	
-	        }
-		}
-		
-		// Generate a random plaintext
-		public String randomPlaintext()
-		{
-			int newPlaintextLength = RandomCharacterGenerator.randomInteger(MINLENGTH_PLAINTEXT, MAXLENGTH_PLAINTEXT, randomGenerator);
-			return RandomCharacterGenerator.generateRandomPlaintext(newPlaintextLength,0,ALPHABETSIZE-1,randomGenerator);
-		}
-		
-		
-		public TreeMap getMicroTable()
-		{
-			return microTable;
-		}
+	private static final int MINLENGTH_PLAINTEXT = 3;
+	private static final int MAXLENGTH_PLAINTEXT = 10;
+	// Using only lowercase letters for testing
+	private static final int ALPHABETSIZE = 26;
 	
-		// Convert byte array to hex values and store it in a string
+	PlaintextSpace pSpace;
+	int blockSize;
+	
+	public RainbowTable(int chainLength, int chainCount, String alphabet, int blockNumber, int textLength)
+	{
+		this.chainCount = chainCount;
+		this.chainLength = chainLength;
+		//this.startingPlaintext = startingPlaintext;
 		
-		public static String getHex( byte [] msgDigest ) {
-	         final StringBuilder hex = new StringBuilder( 2 * msgDigest.length );
-	         for (byte b : msgDigest) {
-	              hex.append(String.format("%02x", b));
-	         }
-	         return hex.toString();
-	     }
-		
-		/*
-		
-		public static String getHex( byte [] msgDigest ) {
-		
-			String HEXVALUES = "0123456789abcdef";
-			if ( msgDigest == null ) {
-				return null;
+		microTable = new TreeMap<String,String>();
+		//visitedPlaintexts = new HashMap<String,Integer>();
+		randomGenerator = new Random();
+		pSpace = new PlaintextSpace(alphabet,blockNumber, textLength);
+		startingPlaintext = pSpace.getText(0);
+		//System.out.println("Starting plaintext --------------> "+startingPlaintext);
+		int lastIndex = PlaintextSpace.BLOCK_SIZE-1;
+		//System.out.println(lastIndex);
+		assert(lastIndex!=-1);
+		endingPlaintext = pSpace.getText(lastIndex);
+		//System.out.println("Ending plaintext --------------> "+endingPlaintext);
+	}
+	
+	public void generateMicroTable() throws NoSuchAlgorithmException
+	{
+		byte msgDigest[] = null;
+		MessageDigest algorithm = MessageDigest.getInstance("MD5");
+		String value;
+		int blockCount = 1;
+		for (int i = 0; i < chainCount; i++) {
+			// Store startingPlaintext in another variable to avoid getting modified
+			value = startingPlaintext;
+			for (int j = 0; j < chainLength; j++) {
+				algorithm.reset();
+				algorithm.update(value.getBytes());
+				msgDigest= algorithm.digest();
+				value = ReductionFunction.reductionFunction(msgDigest, j, pSpace.getText(0), endingPlaintext);
 			}
-			final StringBuilder hex = new StringBuilder( 2 * msgDigest.length );
-			for ( final byte b : msgDigest ) {
-				hex.append(HEXVALUES.charAt((b & 0xF0) >> 4))
-				.append(HEXVALUES.charAt((b & 0x0F)));
+			// Add first plaintext of chain along with the last hash
+			microTable.put(startingPlaintext, getHex(msgDigest));
+			int randomIndex = RandomCharacterGenerator.randomInteger(1, PlaintextSpace.BLOCK_SIZE, randomGenerator);
+			//System.out.println("Random Index: "+randomIndex);
+			startingPlaintext = pSpace.getText(randomIndex);
+			// Keep generating random plaintexts until it finds one that haven't been used
+			while (microTable.containsKey(startingPlaintext))
+			{
+				//blockCount++;
+				randomIndex = RandomCharacterGenerator.randomInteger(1, PlaintextSpace.BLOCK_SIZE, randomGenerator);
+				//System.out.println("Random Index: "+randomIndex);
+				startingPlaintext = pSpace.getText(randomIndex);
+				//startingPlaintext = randomPlaintext();
 			}
-	    return hex.toString();
+			blockCount++;
+		
 		}
+	}
+	
+	// Generate a random plaintext
+	public String randomPlaintext()
+	{
+		int newPlaintextLength = RandomCharacterGenerator.randomInteger(MINLENGTH_PLAINTEXT, MAXLENGTH_PLAINTEXT, randomGenerator);
+		return RandomCharacterGenerator.generateRandomPlaintext(newPlaintextLength,0,ALPHABETSIZE-1,randomGenerator);
+	}
+	
+	
+	public TreeMap getMicroTable()
+	{
+		return microTable;
+	}
+	
+	// Convert byte array to hex values and store it in a string
+	
+	public static String getHex( byte [] msgDigest ) {
+		final StringBuilder hex = new StringBuilder( 2 * msgDigest.length );
+		for (byte b : msgDigest) {
+			hex.append(String.format("%02x", b));
+		}
+		return hex.toString();
+	}
+	
+	/*
+	public static String getHex( byte [] msgDigest ) {
+		String HEXVALUES = "0123456789abcdef";
+		if ( msgDigest == null ) {
+			return null;
+		}
+		final StringBuilder hex = new StringBuilder( 2 * msgDigest.length );
+		for ( final byte b : msgDigest ) {
+			hex.append(HEXVALUES.charAt((b & 0xF0) >> 4))
+			.append(HEXVALUES.charAt((b & 0x0F)));
+		}
+		return hex.toString();
+	}
+	*/
+	
+	// Search a hash from the micro table
+	public String getPlaintextFromHash(String hash) throws NoSuchAlgorithmException
+	{
+		// Storing unique plaintexts
+		HashSet visitedPlaintexts = new HashSet();
+		visitedPlaintexts.add(hash);
 		
-		*/
+		byte msgDigest[] = null;
+		MessageDigest algorithm = MessageDigest.getInstance("MD5");
 		
-		// Search a hash from the micro table
-		public String getPlaintextFromHash(String hash) throws NoSuchAlgorithmException
-		{
-			// Storing unique plaintexts
-			HashSet visitedPlaintexts = new HashSet();
-			visitedPlaintexts.add(hash);
+		boolean found = false;
+		
+		Set set = microTable.entrySet();
+		// Get an iterator
+		Iterator i = set.iterator();
+		
+		String foundValue = null;
+		// Store hash in a new variable to avoid getting modified
+		String newHash = hash;
+		int count = 0;
+		while (!found){
 			
-			byte msgDigest[] = null;
-			MessageDigest algorithm = MessageDigest.getInstance("MD5");
-		       
-			boolean found = false;
+			while(i.hasNext()) {
 			
-			Set set = microTable.entrySet();
-		   	 // Get an iterator
-		    	Iterator i = set.iterator();
-		    
-		    	String foundValue = null;
-		    	// Store hash in a new variable to avoid getting modified
-		    	String newHash = hash;
-			while (!found){
-				 //System.out.println("starting hash: "+newHash);
-			    while(i.hasNext()) {
-			    	
-			    	Map.Entry chain = (Map.Entry)i.next();
-				    // Check if the hash corresponds to on the last hashes of each chain
-				    if (chain.getValue().equals(newHash))
-				    {
-				    	// Store the first plaintext of the chain
-				    	String value = (String) chain.getKey();
-				    	// Using the reduction function to find the right plaintext from the chain
-				    	for (int j = 0; j < 1000; j++) {
+				Map.Entry chain = (Map.Entry)i.next();
+				// Check if the hash corresponds to on the last hashes of each chain
+				if (chain.getValue().equals(newHash))
+				{
+					// Store the first plaintext of the chain
+					String value = (String) chain.getKey();
+					// Using the reduction function to find the right plaintext from the chain
+					for (int j = 0; j < chainLength; j++) {
 						algorithm.reset();
 						algorithm.update(value.getBytes());
 						msgDigest= algorithm.digest();
+						//System.out.println(getHex(msgDigest) + "--------------->"+value);
 						if (getHex(msgDigest).equals(hash))
 						{
 							found = true;
 							foundValue = value;
 							break;
 						}
-						value = ReductionFunction.reductionFunction(msgDigest);
-				     	}
-				     }
-			    }
-			    if (!found) // Hash wasn't found among last hashes from the chains
-			    {
-			    	    // Reduce the hash
-				    String reducedHash = ReductionFunction.reductionFunction(newHash.getBytes());
-				    algorithm.reset();
-				    algorithm.update(reducedHash.getBytes());
-				    msgDigest= algorithm.digest();
-				    // Generate a new hash
-				    newHash = getHex(msgDigest);
-				    // Check if the new hash is not already stored in the HashSet
-				    while (!visitedPlaintexts.add(newHash))
-				    {
-				    	reducedHash = ReductionFunction.reductionFunction(newHash.getBytes());
-				    	
-				    	algorithm.reset();
-					    algorithm.update(reducedHash.getBytes());
-					    msgDigest= algorithm.digest();
-					    
-					   newHash = getHex(msgDigest);
-				    }
-			 
-				    // Reset the iterator
-				    set = microTable.entrySet();
-				    i = set.iterator();
-			    }
-		}
-		return foundValue;
+							value = ReductionFunction.reductionFunction(msgDigest, j, pSpace.getText(0), endingPlaintext);
+						}
+					}
+				}
+				if (!found) // Hash wasn't found among last hashes from the chains
+				{
+					// Reduce the hash
+					if (count>=chainLength)
+					{
+						count = 0;
+					}
+					String reducedHash = ReductionFunction.reductionFunction(newHash.getBytes(),count,pSpace.getText(0), endingPlaintext);
+					algorithm.reset();
+					algorithm.update(reducedHash.getBytes());
+					msgDigest= algorithm.digest();
+					// Generate a new hash
+					newHash = getHex(msgDigest);	
+					int count2 = count;
+					// Check if the new hash is not already stored in the HashSet
+					while (!visitedPlaintexts.add(newHash))
+					{
+						reducedHash = ReductionFunction.reductionFunction(newHash.getBytes(), count2, pSpace.getText(0), endingPlaintext);
+						
+						algorithm.reset();
+						algorithm.update(reducedHash.getBytes());
+						msgDigest= algorithm.digest();
+						
+						newHash = getHex(msgDigest);
+						count2++;
+						if (count2>=chainLength)
+						{
+							count2 = 0;
+						}
+					}
+				
+					// Reset the iterator
+					set = microTable.entrySet();
+					i = set.iterator();
+				}
+				count++;
+			}
+			return foundValue;
 	}
 	
 	public static void main (String[]args) throws NoSuchAlgorithmException
 	{
+	
+		final int CHAIN_LENGTH = 1000;
+		final int CHAIN_COUNT = 1000;
+		
+		// Create a new rainbow table with "helloworld" as starting plaintext
+		// and 1000 chains, with length 1000
+		RainbowTable rt = new RainbowTable(CHAIN_LENGTH,CHAIN_COUNT,(AlphabetGenerator.generateAlphabet(
+				AlphabetGenerator.Types.LOWER_CASE)), 5, 5);
+		
+		long start = 0, end = 0;
+		try {
+		
+			start = System.currentTimeMillis();
 			
-			final int CHAIN_LENGTH = 1000;
-			final int CHAIN_COUNT = 1000;
+			rt.generateMicroTable();
 			
-			// Create a new rainbow table with "helloworld" as starting plaintext
-			// and 1000 chains, with length 1000
-			RainbowTable rt = new RainbowTable(CHAIN_LENGTH,CHAIN_COUNT,"helloworld");
+			end = System.currentTimeMillis();
 			
-			long start = 0, end = 0;
-			try {
-				
-				start = System.currentTimeMillis();
-				
-				rt.generateMicroTable();
-				
-				end = System.currentTimeMillis();
-				
-			} catch (NoSuchAlgorithmException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			 
-			// Format duration to display: xx min yy sec
-		    String duration = String.format("%d min, %d sec", 
-		    	    TimeUnit.MILLISECONDS.toMinutes(end - start),
-		    	    TimeUnit.MILLISECONDS.toSeconds(end - start) - 
-		    	    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(end - start))
-		    	);
-		        
-		    System.out.println("Duration : " + duration);
-		    
-			TreeMap microTable = rt.getMicroTable();
-			Set set = microTable.entrySet();
-		    // Get an iterator
-		    Iterator i = set.iterator();
-		    // Display the micro table
-		    while(i.hasNext()) {
-			    Map.Entry chain = (Map.Entry)i.next();
-			    System.out.print(chain.getKey() + ": ");
-				System.out.println(chain.getValue());
-		    }
-		    
-		    assert(microTable.size() == CHAIN_COUNT);
-		    
-		    System.out.println("Micro table length: "+microTable.size());
-		    
-		    System.out.println("Searching hash: "+"4c47f867a81e9af4fe4c7f89a750a981");
-		    
-		    // Get plaintext from hash (last hash of chain 1)
-		    String plaintext = rt.getPlaintextFromHash("4c47f867a81e9af4fe4c7f89a750a981");
-		    
-		    System.out.println("Found plaintext: "+plaintext);
-	  	  
+		} catch (NoSuchAlgorithmException e) {
+		// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Format duration to display: xx min yy sec
+		String duration = String.format("%d min, %d sec",
+		TimeUnit.MILLISECONDS.toMinutes(end - start),
+		TimeUnit.MILLISECONDS.toSeconds(end - start) -
+		TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(end - start))
+		);
+		
+		System.out.println("Duration : " + duration);
+		
+		TreeMap microTable = rt.getMicroTable();
+		Set set = microTable.entrySet();
+		// Get an iterator
+		Iterator i = set.iterator();
+		// Display the micro table
+		while(i.hasNext()) {
+			Map.Entry chain = (Map.Entry)i.next();
+			System.out.print(chain.getKey() + ": ");
+			System.out.println(chain.getValue());
+		}
+		
+		assert(microTable.size() == CHAIN_COUNT);
+		
+		System.out.println("Micro table length: "+microTable.size());
+		
+		System.out.println("Searching hash: "+"f6a94670a01d5eabd5132818a1957d5f");
+		
+		// Get plaintext from hash (last hash of chain 1)
+		String plaintext = rt.getPlaintextFromHash("f6a94670a01d5eabd5132818a1957d5f");
+		
+		System.out.println("Found plaintext: "+plaintext);
+	
 	}
 
 }
+
 
--- src/ReductionFunction.java
+++ src/ReductionFunction.java
@@ -2,6 +2,9 @@
 import java.math.BigInteger;
 import java.security.MessageDigest;
 
+import rainbow.scheduler.partition.AlphabetGenerator;
+import rainbow.scheduler.partition.PlaintextSpace;
+
 public class ReductionFunction {
 
     public static String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
@@ -14,75 +17,47 @@ public class ReductionFunction {
         }
         return number;
     }
-    /*
-     * Reduction function, just as a test will go from cccccc to hhhhhh Will
-     * return a string from cccccc to hhhhhh
-     */
 
-    public static String reductionFunction(byte[] hash) {
-        return reductionFunction(hash, 0);
-    }
-    public static String begin = "cccccc";
-    public static String end = "hhhhhh";
-
-    public static String reductionFunction(byte[] hash, int reductionNumber) {
-        int[] start = new int[6];
+    public static String reductionFunction(byte[] hash, int reductionNumber, String begin, String end) {
+    	int chars_count = begin.length();
+    	int lastIndex = chars_count-1;
+        int[] start = new int[begin.length()];
         // Add the beginning string into the start array
         long beginValue = toNumber(begin.toCharArray());
-        int index = 5;
+        int index = begin.length()-1;
         while (beginValue > 0) {
             start[index--] = (int) (beginValue % ALPHABET.length());
             beginValue /= ALPHABET.length();
         }
-        //calculate how many strings there are from aaaaa to mmmmm        
-        long blockLength = toNumber(end.toCharArray()) - toNumber(begin.toCharArray());
+        //calculate how many strings there are from begin to end      
+        long blockLength = Math.abs(toNumber(end.toCharArray()) - toNumber(begin.toCharArray()));
         // convert hash to 128bit integer
         BigInteger value = new BigInteger(hash);
         // Add in the reduction number to have different functions
         value = value.add(BigInteger.valueOf(reductionNumber));
         // Mod by the length of the block so we have values from 0 to blockLength
         value = value.mod(BigInteger.valueOf(blockLength));
+        
         // Convert back to long for easier calculations
         long newValue = value.longValue();
         // Do a divide/mod loop to pull out individual letters
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < chars_count; i++) {
             int offset = (int) (newValue % ALPHABET.length());
             newValue = newValue / ALPHABET.length();
             // Add to start array
-            start[5 - i] += offset;
+            start[lastIndex - i] += offset;
             // Check if we have to "carry" to the next significant bit
-            if (start[5 - i] >= ALPHABET.length()) {
-                start[5 - i] -= ALPHABET.length();
-                start[5 - i - 1]++;
+            if (start[lastIndex - i] >= ALPHABET.length()) {
+                start[lastIndex - i] -= ALPHABET.length();
+                start[lastIndex - i - 1]++;
             }
         }
         // Generate the output string
         String output = "";
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < chars_count; i++) {
             output += ALPHABET.charAt(start[i]);
         }
 
         return output;
-    }
-
-    public static void main(String[] s) {
-        try {
-
-            MessageDigest algorithm = MessageDigest.getInstance("MD5");
-            // Run 1000 tests, should not get anything where the first letter is past h or before c
-            // Since the range is from cccccc to hhhhhh
-            for (int i = 0; i < 1000; i++) {
-                algorithm.reset();
-                algorithm.update(("teststring" + i).getBytes());
-                byte msgDigest[] = algorithm.digest();
-                String value = reductionFunction(msgDigest);
-                System.out.println(value);
-                if (value.charAt(0) > 'h' || value.charAt(0) < 'c') {
-                    throw new RuntimeException();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
